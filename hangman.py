74e29955d1c2abac91b12583ebd3039e5ef68c2d
Cole Michael <your.email@example.com>
2026-03-09 11:00:07 -0400

    chat like this dict better

--- hangman.py
+++ hangman.py
@@ -54,36 +54,9 @@ class ComputerPlayer(Player):
         self.vocab = noun_list
         
     def turn(self, gstate):
-        vowels = {
-                    'a': 0,
-                    'e': 0,
-                    'i': 0,
-                    'o': 0,
-                    'u': 0
-                }
-        consonants = {
-            'b': 0,
-            'c': 0,
-            'd': 0,
-            'f': 0,
-            'g': 0,
-            'h': 0,
-            'j': 0,
-            'k': 0,
-            'l': 0,
-            'm': 0,
-            'n': 0,
-            'p': 0,
-            'q': 0,
-            'r': 0,
-            's': 0,
-            't': 0,
-            'v': 0,
-            'w': 0,
-            'x': 0,
-            'y': 0,
-            'z': 0
-        }
+        consonants = {c: 0 for c in "bcdfghjklmnpqrstvwxyz"}
+        vowels = {v: 0 for v in "aeiou"}
+        
         for word in self.vocab:
             if len(word) == len(gstate.letter_count):
                 for c in word:       
